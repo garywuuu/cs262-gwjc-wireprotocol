d05541934040486161c0a9ef6497a8c82fbe81db
Gary <garywu@college.harvard.edu>
2023-02-16 13:45:23 -0500

    Restructured + some other things working, trying to get detect disconnect working

--- server.py
+++ server.py
@@ -9,76 +9,80 @@ active_users = []
 
 undelivered_messages = {}
 
-# do everything by username and address
-def threaded(user, addr):
-	msg = "Hello there"
-	user.send(msg.encode('ascii'))
+# do everything by user info and username
+def threaded(user):
+	while True:
+		username = ""
+		for k, v in clients.items():
+			if user == v:
+				username = k
+				if k in active_users:
+					pass
+				else:
+					active_users.append(k)
+		try:
+			data = (user.recv(1024)).decode('UTF-8')
+			data_list = data.split('|')	
+			task = data_list[0]
+			
+			check_operations(task, user, data_list, username)
+
+		except socket.error as e:
+			active_users.pop(username)
+			user.close()
+			print(e)
+
+def check_operations(task, user, data_list, username):
+	if task == "1":
+		# checks to make sure not taken etc
+		username = data_list[1]
+		print(username)
+		dupe = False
+		if username in clients.keys() or user in clients.values():
+			msg = "User already exists, create new account."
+			user.send(msg.encode('UTF-8'))
+			dupe = True
+		if dupe == False:
+			clients[username] = user
+			undelivered_messages[username] = []
+			print(username + " added to server!")
+			msg = "Welcome to Chat262, " + username +"!"
+			user.send(msg.encode('UTF-8'))
+			active_users.append(username)
+
+	elif task == "2":
+		list_accounts(user)
+		print("Listed users")
+
+	elif task == "3":
+		if len(data_list) != 3:
+			msg = "Invalid Command"
+			user.send(msg.encode('UTF-8'))
+			return False
+		print("valid command")
+		receiver_username = data_list[1]
+		message = "From " + username + ": "+ data_list[2] 
+		message = message.encode('UTF-8')
+		if receiver_username in clients.keys():
+			if receiver_username in active_users:
+				sendmessage(message, receiver_username)
+				print("sent message")
+			else:
+				undelivered_messages[username].append(message)
+		else:
+			print("recipient not a user")
+
+	elif task == "4":
+		remove(username)
+
+def deliver_undelivered(user):
 	username = ""
 	for k, v in clients.items():
 		if user == v:
-			active_users.append(k)
 			username = k
+	for msg in undelivered_messages[username]:
+		sendmessage(msg, username)
 
-	while True:
-		data = user.recv(1024)
-		data_str = data.decode('ascii')
-
-		if not data:
-			print("No message")
-			break
-
-		data_list = data_str.split('|')	
-
-		task = data_list[0]
-		
-		if task == "1":
-			# checks to make sure not taken etc
-			username = data_list[1]
-			print(username)
-			dupe = False
-			# for u, info in clients.items():
-			# 	if user == info:
-			# 		msg = "User already exists."
-			# 		user.send(msg.encode('ascii'))
-			# 		dupe = True
-			if dupe == False:
-				clients[username] = user
-				print(username + " added to server!")
-				msg = "Welcome to Chat262, " + username +"!"
-				user.send(msg.encode('ascii'))
-				active_users.append(username)
-
-		elif task == "2":
-			list_accounts(user)
-			print("Listed users")
-
-		elif task == "3":
-			if len(data_list) != 3:
-				msg = "Invalid Command"
-				user.send(msg.encode('ascii'))
-				return False
-			print("valid command")
-			receiver_username = data_list[1]
-			message = "From " + username + ": "+ data_list[2] 
-			message = message.encode('ascii')
-			if receiver_username in clients.keys():
-				if receiver_username in active_users:
-					sendmessage(message, receiver_username)
-					print("sent message")
-				else:
-				#add to queue of unread messages
-					pass
-			else:
-				print("recipient not a user")
-
-		elif task == "4":
-			remove(username)
-
-		else:
-			msg = "Invalid Command"
-			user.send(msg.encode('ascii'))
-
-			
 
 def sendmessage(message, receiver_username):
 	try:
@@ -90,13 +94,14 @@ def sendmessage(message, receiver_username):
 def list_accounts(recipient):
 	users = "Users = "+''.join(str(n)+" | " for n in active_users)
 	print(users)
-	recipient.send(users.encode('ascii'))
+	recipient.send(users.encode('UTF-8'))
 
 
 def remove(username):
 	if username in clients.keys():
 		clients.pop(username)
 		active_users.pop(username)
+		clients[username].close()
 
 
 def Main():
@@ -106,7 +111,7 @@ def Main():
 
 	# takes the first argument from command prompt as IP address
 	IP = "127.0.0.1"
-	port=5000
+	port=5001
 	# takes second argument from command prompt as port number
 
 	server.bind((IP, port))
@@ -123,7 +128,7 @@ def Main():
 		print('Connected to :', addr[0], ':', addr[1])
 
 		# Start a new thread and return its identifier
-		start_new_thread(threaded, (user, addr[0]))
+		start_new_thread(threaded, (user,))
 		
 	server.close()
 
--- yt.py
+++ yt.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 5000))
+client.connect(('127.0.0.1', 5001))
 
 
 def client_receive():
